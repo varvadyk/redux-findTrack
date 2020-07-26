66528eae304893eb57950ed7529767b15d30f517
Diana <diana_ivanivna@kr.net>
2020-07-26 11:21:08 +0300

    realization findTracks in redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reducer from './reducers';
+
+
+
+const store =  createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={ store}>
+     <App />
+  </Provider>,
+   
+
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
